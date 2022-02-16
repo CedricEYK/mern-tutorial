e55a5360fcad5ff6e27aa50f8fff3b4db28db72a
Cedric.K <cedric.e.y.k@hotmail.com>
2022-02-16 17:32:58 +0100

    connect database

---
+++ backend/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectMongoDBAtlas = async (listenForPort) => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      // useUnifinedTopology: true,
+      // unseFindAndModify: false,
+    });
+
+    console.log(`MongoDb Atlas Connected: ${conn.connection.host}`);
+    listenForPort();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectMongoDBAtlas;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("Hey!");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const connectDataBase = require("./config/database");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.status(200).json({
+    msg: "Hey!",
+  });
+});
+
+connectDataBase(() => {
+  app.listen(
+    port,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+    )
+  );
+});
