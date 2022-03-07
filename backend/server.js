ab24706f4c1259cdb4f2b66c728f1621bd2d1487
Cedric.K <cedric.e.y.k@hotmail.com>
2022-03-07 15:03:54 +0100

    production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { urlencoded } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -23,6 +24,18 @@ app.use(morgan('dev'));
 //* Routes
 app.use(require('./routes/goals'));
 app.use(require('./routes/users'));
+
+//*Serve the frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
 
